ae99c13986ea2d81a77a067a5ee7c6e0928a3717
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-06-02 02:57:01 -0700

    Gamepad Metadata Renderer
    Got the reactive part of the rendering working fairly nicely actually! I think the concept to look into now though is to split the updates into proper single state parts, because right now they all cross-update each other, which is a bit unfortunate.
    
    I wonder if it makes sense to abstract the underlying Gamepad API into an event-based one, and then add SolidJS on top of that, for rendering only, or to just use Solid's state tools to manage it on it's own? I thought that second one could do that very nicely, but it still seems to have the issues I kind of thought it would have. Dang every time I work with this API, it's just not quite what I'd expect it to work like. That's okay though! It takes time :) There's a reason they built it this way I think.
    
    I also learned that my SteelSeries Nimbus shows as two separate controllers because one is for the home button only, and the rest are the regular controller buttons. Definitely interesting.

--- src/Gamepads.tsx
+++ src/Gamepads.tsx
@@ -1,6 +1,8 @@
 import { createEffect } from "solid-js";
 import { createStore } from "solid-js/store";
 
+import type { Component } from "solid-js";
+
 export type Gamepads = ReturnType<typeof navigator.getGamepads>;
 
 export default function Gamepad() {
@@ -22,6 +24,41 @@ export default function Gamepad() {
   }
 
   return (
-    <>{gamepads.map(gamepad => gamepad)}</>
+    <>{
+      gamepads
+        .filter((gamepad): gamepad is Gamepad => gamepad !== null)
+        .map(gamepad => <GamepadRender gamepad={gamepad}/>)
+    }</>
   );
 }
+
+const GamepadRender: Component<{ gamepad: Gamepad; }> = (props) => {
+  return (
+    <pre style={{ "font-size": "10px" }}>
+      <fieldset>
+        Axes: {props.gamepad.axes.join(", ")}
+      </fieldset>
+      <fieldset>
+        Buttons: {props.gamepad.buttons.map(({ pressed, touched, value }) => JSON.stringify({ pressed, touched, value })).join("\n")}
+      </fieldset>
+      <fieldset>
+        Connected: {props.gamepad.connected}
+      </fieldset>
+      <fieldset>
+        ID: {props.gamepad.id}
+      </fieldset>
+      <fieldset>
+        Index: {props.gamepad.index}
+      </fieldset>
+      <fieldset>
+        Mapping: {props.gamepad.mapping}
+      </fieldset>
+      <fieldset>
+        Timestamp: {props.gamepad.timestamp}
+      </fieldset>
+      <fieldset>
+        Vibration Actuator: {JSON.stringify(props.gamepad.vibrationActuator)}
+      </fieldset>
+    </pre>
+  );
+};
