830ffe7cb166529c01c0df817df0c4aa3581de9b
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-06-02 01:09:00 -0700

    Gamepad State Looper
    Not working as intended. I think the concept is right though, I just don't quite click with how to set it up nicely. Something like `createGamepads()` would be awesome!
    
    https://www.solidjs.com/tutorial/lifecycles_oncleanup?solved
    https://css-tricks.com/using-abortcontroller-as-an-alternative-for-removing-event-listeners/
    https://www.linkedin.com/pulse/exploring-code-48-using-abort-controller-easily-remove-gabriel-thurau-rthpf/
    https://developer.mozilla.org/en-US/docs/Web/API/AbortController/abort
    https://github.dev/Offroaders123/Flatlands
    https://www.solidjs.com/docs/latest/api#onmount
    https://dev.to/aderchox/lets-learn-solidjs-quickly-by-creating-a-usedebounce-hook-3hf0

--- src/Gamepads.tsx
+++ src/Gamepads.tsx
@@ -1,4 +1,4 @@
-import { createEffect } from "solid-js";
+import { createEffect, onCleanup, onMount } from "solid-js";
 import { createStore } from "solid-js/store";
 
 export type Gamepads = ReturnType<typeof navigator.getGamepads>;
@@ -6,13 +6,32 @@ export type Gamepads = ReturnType<typeof navigator.getGamepads>;
 export default function Gamepad() {
   const [gamepads, setGamepads] = createStore<Gamepads>(navigator.getGamepads());
 
+  const controller = new AbortController();
+  const { signal } = controller;
+
   window.addEventListener("gamepadconnected", event => {
     console.log(event);
+  }, { signal });
+
+  onMount(() => {
+    loop();
+  });
+
+  onCleanup(() => {
+    controller.abort();
   });
 
   createEffect(() => {
-    console.log(gamepads);
+    console.log(
+      gamepads
+        .filter((gamepad): gamepad is Gamepad => gamepad !== null)
+        .map(gamepad => gamepad.axes)
+    );
   });
+
+  function loop() {
+    requestAnimationFrame(loop);
+  }
 
   return (
     <>{gamepads.map(gamepad => gamepad)}</>
