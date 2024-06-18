b6c2f7df628e58eee010c86386a84365f5e6aa42
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-06-17 20:27:40 -0700

    GameController.js Demo
    This is still kind of a big mix of gunk, it's not working how I'd like it to.
    
    To get it working, you also have to cd into the node_modules folder for the new controller lib, and build it with `npm run build`. This commit is more for a conceptual setup of things.

--- src/Gamepads.tsx
+++ src/Gamepads.tsx
@@ -1,64 +1,29 @@
-import { createEffect } from "solid-js";
-import { createStore } from "solid-js/store";
+import { onMount } from "solid-js";
+import "gamecontroller.js";
 
-import type { Component } from "solid-js";
+import type GameControl from "gamecontroller.js/dist/gamecontrol.js";
+import type { GamepadPrototype } from "gamecontroller.js/dist/gamepad.js";
 
-export type Gamepads = ReturnType<typeof navigator.getGamepads>;
-
-export default function Gamepad() {
-  const [gamepads, setGamepads] = createStore<Gamepads>(navigator.getGamepads());
-
-  loop();
-
-  createEffect(() => {
-    console.log(
-      ...gamepads
-        .filter((gamepad): gamepad is Gamepad => gamepad !== null)
-        .map(gamepad => gamepad.axes)
-    );
-  });
-
-  function loop() {
-    setGamepads(navigator.getGamepads());
-    requestAnimationFrame(loop);
-  }
-
-  return (
-    <>{
-      gamepads
-        .filter((gamepad): gamepad is Gamepad => gamepad !== null)
-        .map(gamepad => <GamepadRender gamepad={gamepad}/>)
-    }</>
-  );
+declare global {
+  var gameControl: typeof GameControl;
 }
 
-const GamepadRender: Component<{ gamepad: Gamepad; }> = (props) => {
+export default function Gamepad() {
+  let gamepad: GamepadPrototype;
+
+  onMount(() => {
+    gameControl
+      .on("connect", (gamepadd: GamepadPrototype) => {
+        console.log(gamepadd);
+        gamepad = gamepadd;
+
+        gamepad.on("button0", event => {
+          console.log(event);
+        });
+      });
+  });
+
   return (
-    <pre style={{ "font-size": "10px" }}>
-      <fieldset>
-        Axes: {props.gamepad.axes.join(", ")}
-      </fieldset>
-      <fieldset>
-        Buttons: {props.gamepad.buttons.map(({ pressed, touched, value }) => JSON.stringify({ pressed, touched, value })).join("\n")}
-      </fieldset>
-      <fieldset>
-        Connected: {props.gamepad.connected}
-      </fieldset>
-      <fieldset>
-        ID: {props.gamepad.id}
-      </fieldset>
-      <fieldset>
-        Index: {props.gamepad.index}
-      </fieldset>
-      <fieldset>
-        Mapping: {props.gamepad.mapping}
-      </fieldset>
-      <fieldset>
-        Timestamp: {props.gamepad.timestamp}
-      </fieldset>
-      <fieldset>
-        Vibration Actuator: {JSON.stringify(props.gamepad.vibrationActuator)}
-      </fieldset>
-    </pre>
+    <></>
   );
-};
+}
